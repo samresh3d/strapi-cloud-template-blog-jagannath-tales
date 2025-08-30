3277f76bc7ae16ac20177a1e2051c09488f11ddf
Samresh Pathak <samresh3d@gmail.com>
2025-08-30 18:02:54 +0530

    Configure GraphQL, SEO, Import/Export, and Responsive Image plugins

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,32 @@
-module.exports = () => ({});
+module.exports = () => ({
+  graphql: {
+    enabled: true,
+    config: {
+      defaultLimit: 10,
+      maxLimit: 100,
+    }
+  },
+  'import-export-entries': {
+    enabled: true,
+    config: {
+      // By default, the serverless image optimization is on
+      serverlessImageOptimization: true,
+    }
+  },
+  seo: {
+    enabled: true,
+  },
+  'responsive-image': {
+    enabled: true,
+    config: {
+      formats: ["webp", "jpeg"],
+      breakpoints: {
+        large: 1000,
+        medium: 750,
+        small: 500,
+        thumbnail: 150
+      },
+      quality: 80,
+    }
+  },
+});
